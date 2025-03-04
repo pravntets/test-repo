e8674ee77c2c7af07786caf9f8935d3d2b5da556
pravntets <praveen@theeminence.org>
2025-03-04 18:15:48 +0530

    Revert "Reapply "Reapply "to be reverted"""
    This reverts commit 7a7ed438db1a4a6533fd5d5a70b8258e432e82d6.

--- tests/branch-test.spec.ts
+++ tests/branch-test.spec.ts
@@ -1,6 +1,3 @@
 const newBranch = "New changes";
 const tostash = "to be Stached";
 const check = "to be stached";
-const dfd = "to be stached";
-const chsdsdeck = "to be stached";
-const chesddck = "to be stached";
